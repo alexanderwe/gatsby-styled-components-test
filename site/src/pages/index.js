dfc4ee823f69277b582318d01268155af0505bfb
Alexander Weiß <weiss_alex@gmx.net>
2019-06-22 09:47:11 +0200

    update

--- site/src/pages/index.js
+++ site/src/pages/index.js
@@ -1,3 +1,15 @@
 import React from "react";
+import styled from "styled-components";
 
-export default () => <div>Hello World!</div>;
+const Wrapper = styled.div`
+  font-size: 32pt;
+  color: black;
+  margin-left: 200px;
+`;
+
+export default () => (
+  <Wrapper>
+    Styles are correctly loaded in index.js but not in layout.js and every other
+    file which is not a "page"
+  </Wrapper>
+);
